fe567b30063baeb09da8c1bda6209177d1a69826
shanAweb <shanasif2002@gmail.com>
2024-08-27 11:49:25 +0500

    Understand ternary operator

---
+++ ternaryOperator.js
@@ -0,0 +1,5 @@
+// Scenerio
+// if a person has 100 points, he is gold otherwise silver
+let personPoints=110;
+let personCategory = personPoints > 100 ? 'gold' : 'silver';
+console.log(personCategory);
