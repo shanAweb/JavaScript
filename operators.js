f3cfabb22679045b882624747cbb46a04e9446eb
shanAweb <shanasif2002@gmail.com>
2024-08-27 11:30:40 +0500

    Understand operators in JS

---
+++ operators.js
@@ -0,0 +1,30 @@
+// Arithmetic Operators
+console.log(5+3);
+console.log(6/2);
+console.log(8%2);
+
+let x = 10;
+let y = 5;
+console.log("value of x: " + x);
+console.log("value of y: "+ y);
+// Increment & Decrement Operators
+// Pre Increment
+console.log("result of pre-increment (x): " + ++x);
+console.log("result of pre-increment (y): " + ++y);
+// Post Increment
+console.log("post-increment started: " + x++);
+console.log("result of post-increment (x): "+ x);
+console.log("post-increment started: " + y++);
+console.log("result of post-increment (y): "+ y);
+
+
+
+// Decrement
+// Pre Increment
+console.log("result of pre-decrement (x): " + --x);
+console.log("result of pre-decrement (y): " + --y);
+// Post Increment
+console.log("post-decrement started: " + x--);
+console.log("result of post-decrement (x): "+ x);
+console.log("post-decrement started: " + y--);
+console.log("result of post-decrement (y): "+ y);
