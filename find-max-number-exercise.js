f28e4aeb8bd8ff9e7bb4a417b00fbd3d4ba34070
shanAweb <shanasif2002@gmail.com>
2024-09-21 17:25:21 +0500

    find max number using a function; js exercise

---
+++ find-max-number-exercise.js
@@ -0,0 +1,9 @@
+function findmaxnumber(number1, number2){
+    if (number1 > number2){
+        return number1;
+    }
+    else{
+        return number2;
+    }
+}
+console.log(findmaxnumber(5,6));
